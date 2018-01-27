6c4271c833bf2b12903a29f23ceffb343ac0e85d
Michael Arnold <michaelthearnold@gmail.com>
2018-01-26 17:04:31 -0800

    about to build server

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        APP GOES HERE
+      </div>
+    );
+  }
+}
+
+export default App;
